d294ece8bee1d837f086b643faab0a430f7a83f7
PedroTepedino <pedrotepedinossauro@gmail.com>
2023-10-11 20:57:47 -0300

    Add faturamentos por categoria, por mes e acumulados

--- faturamento.R
+++ faturamento.R
@@ -23,10 +23,31 @@ vendas_price_na %>%
   group_by(Category) %>%
   summarize(total = sum(Price))
 
+# faturamento total por mes
 vendas_price_na %>%
-  group_by(lubridate::floor_date(Data.Venda, "month")) %>%
+  group_by(month = month(floor_date(Data.Venda, "month"))) %>%
   summarize(total = sum(Price))
 
+# faturamento total por mes por categoria
 vendas_price_na %>%
-  group_by(Category, lubridate::floor_date(Data.Venda, "month")) %>%
+  group_by(Category, month = month(floor_date(Data.Venda, "month"))) %>%
   summarize(total = sum(Price))
+
+# faturamento acumulado total por mes
+vendas_price_na %>%
+  group_by(month = month(floor_date(Data.Venda, "month"))) %>%
+  summarize(total = sum(Price)) %>%
+  filter(!is.na(month)) %>%
+  mutate(accumutalive = cumsum(total))
+  
+# faturamento acumulado por categoria por mes
+vendas_price_na %>%
+  group_by(Category, month = month(floor_date(Data.Venda, "month"))) %>%
+  summarize(total = sum(Price)) %>%
+  filter(!is.na(month)) %>%
+  filter(!is.na(Category)) %>%
+  mutate(accumutalive = cumsum(total)) %>%
+  print(n = 50)
+
+
+
