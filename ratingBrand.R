d9754df4cd615a4327d8da10d17d5329d818b6bc
Pedro Tepedino <pedrotepedinossauro@gmail.com>
2023-11-21 15:42:50 -0300

    update analise 6

--- ratingBrand.R
+++ ratingBrand.R
@@ -1,15 +1,13 @@
 source("leituraLimpesaDados.R")
 
+library(xtable)
+
 vendas %>% 
-  filter(!is.na(Brand), !is.na(Rating)) %>%
+  filter(!is.na(Rating)) %>%
   {shapiro.test(.$Rating)}
 
 vendas %>% 
-  filter(!is.na(Brand), !is.na(Rating)) %>%
-  {hist(.$Rating)}
-
-vendas %>%
-  filter(!is.na(Brand), !is.na(Rating)) %>%
+  filter(!is.na(Rating)) %>%
 ggplot() +
   aes(x = Rating) +
   geom_histogram(colour = "white", fill = "#A11D21", binwidth = 0.25) +
@@ -34,7 +32,7 @@ vendas %>%
 vendas %>% 
   filter(!is.na(Brand), !is.na(Rating)) %>%
   group_by(Brand) %>%
-  summarise(mean = mean(Rating))
+  summarise(mean = mean(Rating), sd = sd(Rating), var = var(Rating))
 
 model <-
   vendas %>% 
@@ -45,3 +43,19 @@ summary(model)
 vendas %>%
   filter(!is.na(Rating), !is.na(Brand)) %>%
   bartlett.test(Rating ~ Brand, data = . ) 
+
+quadro_resumo <- vendas %>%
+  filter(!is.na(Rating), !is.na(Brand)) %>%
+  group_by( Brand ) %>% # caso mais de uma categoria
+  summarize (Média = round(mean(Rating),2),
+             `Desvio Padrão ` = round(sd(Rating),2),
+             `Variância ` = round(var(Rating),2),
+             `Mínimo ` = round(min(Rating),2),
+             `1º Quartil ` = round( quantile (Rating , probs = .25),2),
+             Mediana = round( quantile (Rating , probs = .5),2),
+             `3º Quartil ` = round( quantile (Rating , probs = .75),2),
+             `Máximo ` = round(max(Rating),2)) %>% 
+  t() %>% 
+  as.data.frame() 
+
+xtable::xtable(quadro_resumo)
