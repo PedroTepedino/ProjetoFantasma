896337ac89666a61c9219926d504922d8bc53617
Pedro Tepedino <pedrotepedinossauro@gmail.com>
2023-11-22 20:50:11 -0300

    update analise 1

--- faturamento.R
+++ faturamento.R
@@ -11,16 +11,23 @@ vendas_price_na <- filter(vendas_price_na, !is.na(vendas_price_na$Category))
 vendas_price_na$Price %>% sum()
 
 # total por categoria
-vendas_price_na %>% 
+vendas %>%
+  distinct(Product.ID, .keep_all = T) %>%
+  filter(!is.na(Price)) %>%
   group_by(Category) %>%
-  summarize(total = sum(Price))
+  summarize(total = sum(Price)) %>%
+  mutate(perc = {total / sum(total)} %>% percent()) %>%
+  {.$total %>% sum()}
+
 
 # faturamento total por mes por categoria
 faturamento_categoria <-
-vendas_price_na %>%
+vendas %>%
+  distinct(Product.ID, .keep_all = T) %>%
+  filter(!is.na(Price), !is.na(Category)) %>%
   group_by(Category, month = floor_date(Data.Venda, "month")) %>%
-  summarize(total = sum(Price)) %>%
-  filter(!is.na(Category))
+  summarize(total = sum(Price)) 
+
 
 # faturamento acumulado
 faturamento_categoria <-
@@ -38,20 +45,23 @@ faturamento_categoria %>%
   group_by(month) %>%
   summarise(mean = mean(total), sd = sd(total))
 
+# Aparentemente meu sistema operacional nao quis colocar os meses com letra maiuscula, esse foi o jeito...
+month_abreviation <- c( "Dez", "Jan", "Fev", "Mar", "Abr", "Mai", "Jun", "Jul", "Ago", "Set", "Out", "Nov")
+# Por algum motivo pior ainda, ele nao colocou na ordem certa
+#TODO: Arrumar um jeito melhor de concertar isso no futuro
 
-# Plots
-ggplot(faturamento_categoria) +
+vendas %>%
+  distinct(Product.ID, .keep_all = T) %>%
+  filter(!is.na(Price), !is.na(Category)) %>%
+  group_by(Category, month = floor_date(Data.Venda, "month")) %>%
+  summarize(total = sum(Price)) %>%
+  filter(!is.na(month)) %>%
+ggplot() +
   aes(x=month, y=total, group=Category, colour=Category) +
   geom_line(linewidth=1) +
-  labs(x="Mes", y="Faturamento Mensal", colour="Categoria") +
-  scale_x_date(date_breaks = "1 month", date_labels = "%b") +
+  labs(x="Mês", y="Faturamento Mensal", colour="Categoria") +
+  scale_y_continuous(breaks = seq(0, 4000, 500)) +
+  scale_x_date(date_breaks = "1 month", date_labels = month_abreviation) +
   theme_estat()
 ggsave("faturamento_mes_categoria.pdf", width = 158, height = 93, units = "mm")
 
-ggplot(faturamento_categoria) +
-  aes(x=month, y=accumulative, group=Category, colour=Category) +
-  geom_line(linewidth=1) +
-  labs(x="Mes", y="Faturamento Mensal") +
-  scale_x_date(date_breaks = "1 month", date_labels = "%b") +
-  theme_estat()
-ggsave("faturamento_mes_categoria_acumulado.pdf", width = 158, height = 93, units = "mm")
